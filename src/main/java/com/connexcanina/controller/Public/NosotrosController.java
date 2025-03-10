a0c89178210332bbc9533e20ed842731e240972f
Justin Berger <jusaleberger@gmail.com>
2025-03-10 14:51:36 -0600

    Creación Vista Nosotros
    Se creo la Vista Nosotros, con su respectivo diseño y Controlador

---
+++ src/main/java/com/connexcanina/controller/Public/NosotrosController.java
@@ -0,0 +1,16 @@
+package com.connexcanina.controller.Public;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NosotrosController {
+
+    @GetMapping("/nosotros")
+    public String nosotros(HttpServletRequest request, Model model) {
+        model.addAttribute("currentURI", request.getRequestURI());
+        return "nosotros/nosotros";
+    }
+}
