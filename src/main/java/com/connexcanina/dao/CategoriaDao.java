802da5b57569605a51b39d1ce591d10359c8f447
Justin Berger <jusaleberger@gmail.com>
2025-03-22 18:22:49 -0600

    Agregando security tag
    Se agrega el security tag a las páginas principales y se eliminan clases de Servicio y Producto que no serán usadas

--- src/main/java/com/connexcanina/dao/CategoriaDao.java
+++
@@ -1,9 +0,0 @@
-package com.connexcanina.dao;
-
-import com.connexcanina.domain.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoriaDao extends JpaRepository <Categoria,Long>{
-    
-}
--- src/main/java/com/connexcanina/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.connexcanina.dao;
-
-import com.connexcanina.domain.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ProductoDao extends JpaRepository <Producto,Long>{
-    
-}
